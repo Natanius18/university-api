0e9bbf26a1bf695c0abb647c8c791d2c6bc155b6
Natan <59052957+Natanius18@users.noreply.github.com>
2022-05-20 16:05:53 +0300

    Add files via upload

---
+++ src/test/java/software/sigma/internship/UniversityApplicationTests.java
@@ -0,0 +1,13 @@
+package software.sigma.internship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
