f70836760ad1ae3879152f519e2be6316452b939
Natanius18 <leonidchachko17@gmail.com>
2022-06-21 11:13:24 +0300

    EDC-31 Add swagger UI

--- src/main/java/software/sigma/internship/UniversityApplication.java
+++ src/main/java/software/sigma/internship/UniversityApplication.java
@@ -1,11 +1,13 @@
 package software.sigma.internship;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-@EnableTransactionManagement
+@EnableTransactionManagement@OpenAPIDefinition(info = @Info(title = "University API", version = "1.0", description = "Program to create and pass tests"))
 public class UniversityApplication {
 
 	public static void main(String[] args) {
