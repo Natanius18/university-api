3a620fadf74404ffdfb51e9cf7eaee960f3ff4c3
Natan <59052957+Natanius18@users.noreply.github.com>
2022-06-21 12:30:05 +0300

    EDC-36 Fix bugs with CRUD operations (#10)
    EDC-36 Fix bugs with CRUD operations

--- src/main/java/software/sigma/internship/service/impl/TestServiceImpl.java
+++ src/main/java/software/sigma/internship/service/impl/TestServiceImpl.java
@@ -3,9 +3,12 @@ package software.sigma.internship.service.impl;
 import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import software.sigma.internship.dto.QuestionDto;
 import software.sigma.internship.dto.TeacherDto;
 import software.sigma.internship.dto.TestDto;
+import software.sigma.internship.entity.Answer;
+import software.sigma.internship.entity.Question;
 import software.sigma.internship.entity.Teacher;
 import software.sigma.internship.entity.Test;
 import software.sigma.internship.repo.TestRepository;
@@ -13,7 +16,9 @@ import software.sigma.internship.service.QuestionService;
 import software.sigma.internship.service.TestService;
 import software.sigma.internship.validator.exception.TestNotFoundException;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -30,7 +35,6 @@ public class TestServiceImpl implements TestService {
                 .map(entity -> {
                     TestDto testDto = mapper.map(entity, TestDto.class);
                     testDto.setQuestions(null);
-                    testDto.getTeacher().setTests(null);
                     return testDto;
                 }).collect(Collectors.toList());
     }
@@ -51,21 +55,13 @@ public class TestServiceImpl implements TestService {
     public TestDto findById(Long id) {
         Test test = testRepository.findById(id).orElseThrow(() -> new TestNotFoundException(id));
         TestDto testDto = mapper.map(test, TestDto.class);
-        List<QuestionDto> questions = testDto.getQuestions()
-                .stream()
-                .map(question -> {
-                    question.setAnswers(null);
-                    return question;
-                })
-                .collect(Collectors.toList());
-        testDto.setQuestions(questions);
         test.getTeacher().setTests(null);
         return testDto;
     }
 
     @Override
     public QuestionDto findQuestion(Long id, Long qId) {
-        if (testRepository.existsById(id)){
+        if (testRepository.existsById(id)) {
             return questionService.findById(qId);
         }
         throw new TestNotFoundException(id);
@@ -73,17 +69,76 @@ public class TestServiceImpl implements TestService {
 
 
     @Override
+    @Transactional
     public TestDto save(TestDto testDto) {
-        Long id = testDto.getId();
-        if (id == null || testRepository.existsById(id)) {
-            Test newTest = testRepository.save(mapper.map(testDto, Test.class));
-            return mapper.map(newTest, TestDto.class);
+        Long testId = testDto.getId();
+        Optional<Test> existingTest = Optional.empty();
+        if (testId != null) {
+            existingTest = testRepository.findById(testId);
+            if (existingTest.isEmpty()) {
+                throw new TestNotFoundException(testId);
+            }
         }
-        throw new TestNotFoundException(id);
+        Test entity = mapper.map(testDto, Test.class);
+        List<Question> questions = mapQuestions(testDto, entity);
+        entity.setQuestions(questions);
+        deleteQuestionsThatAreNoLongerBelongToTheTest(existingTest, questions);
+        Test newTest = testRepository.save(entity);
+        return mapper.map(newTest, TestDto.class);
     }
+
 
     @Override
     public void deleteById(Long id) {
         testRepository.deleteById(id);
+    }
+
+    private void deleteQuestionsThatAreNoLongerBelongToTheTest(Optional<Test> existingTest, List<Question> newQuestions) {
+        if (existingTest.isPresent()) {
+            List<Long> idsOfNewQuestions = getIdsOfQuestions(newQuestions);
+            List<Long> idsOfOldQuestions = getIdsOfQuestions(existingTest.get().getQuestions());
+            questionService.deleteQuestionByIdIn(getIdsToDelete(idsOfNewQuestions, idsOfOldQuestions));
+        }
+    }
+
+    private List<Long> getIdsOfQuestions(List<Question> questions) {
+        return questions
+                .stream()
+                .map(Question::getId)
+                .collect(Collectors.toList());
+    }
+
+
+    private List<Question> mapQuestions(TestDto testDto, Test entity) {
+        return testDto.getQuestions()
+                .stream()
+                .map(question -> {
+                    Question entityQuestion = mapper.map(question, Question.class);
+                    entityQuestion.setTest(entity);
+                    List<Answer> answers = mapAnswers(question, entityQuestion);
+                    entityQuestion.setAnswers(answers);
+                    return entityQuestion;
+                }).collect(Collectors.toList());
+    }
+
+    private List<Answer> mapAnswers(QuestionDto question, Question entityQuestion) {
+        return question.getAnswers().stream()
+                .map(answer -> {
+                    Answer entityAnswer = mapper.map(answer, Answer.class);
+                    entityAnswer.setQuestion(entityQuestion);
+                    return entityAnswer;
+                }).collect(Collectors.toList());
+    }
+
+
+    public List<Long> getIdsToDelete(List<Long> idsOfNewQuestions, List<Long> idsOfOldQuestions) {
+        List<Long> result = new ArrayList<>(idsOfNewQuestions);
+        result.addAll(idsOfOldQuestions);
+
+        List<Long> intersection = new ArrayList<>(idsOfNewQuestions);
+        intersection.retainAll(idsOfOldQuestions);
+
+        result.removeAll(intersection);
+        return result;
     }
 }
