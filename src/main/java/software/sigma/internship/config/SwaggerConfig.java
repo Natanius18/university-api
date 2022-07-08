2f16dcb5db484a3429ae717b986676341da2c975
Natanius18 <leonidchachko17@gmail.com>
2022-07-08 19:06:13 +0300

    EDC-60 Add authorization to Swagger

--- src/main/java/software/sigma/internship/config/SwaggerConfig.java
+++ src/main/java/software/sigma/internship/config/SwaggerConfig.java
@@ -7,22 +7,21 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.List;
 
 @EnableWebMvc
 @Configuration
 public class SwaggerConfig {
-    @Bean
-    public Docket createRestApi() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiInfo())
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("software.sigma.internship"))
-                .paths(PathSelectors.any())
-                .build();
-    }
+
+    public static final String AUTHORIZATION_HEADER = "Authorization";
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
@@ -31,6 +30,36 @@ public class SwaggerConfig {
                 .description("This API allows teachers to create tests and students to pass these tests")
                 .version("1.0")
                 .build();
+    }
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .securityContexts(List.of(securityContext()))
+                .securitySchemes(List.of(apiKey()))
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("software.sigma.internship"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", AUTHORIZATION_HEADER, "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .build();
+    }
+
+    List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope
+                = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return List.of(new SecurityReference("JWT", authorizationScopes));
     }
 
 }
