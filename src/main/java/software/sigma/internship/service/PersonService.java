66d27a9aded561f6e7f70f1b42760ac8faa17301
Natanius18 <leonidchachko17@gmail.com>
2022-07-23 17:23:31 +0300

    EDC-69 Add java docs

--- src/main/java/software/sigma/internship/service/PersonService.java
+++ src/main/java/software/sigma/internship/service/PersonService.java
@@ -3,6 +3,16 @@ package software.sigma.internship.service;
 import software.sigma.internship.dto.PersonDto;
 import software.sigma.internship.enums.Role;
 
+/**
+ * Interface for approving a role of a person.
+ * @author natanius
+ */
 public interface PersonService {
+    /**
+     * Set a role to the person by email.
+     * @param email the email of the person.
+     * @param role the role to be assigned to the person.
+     * @return person with the new role assigned.
+     */
     PersonDto approve(String email, Role role);
 }
