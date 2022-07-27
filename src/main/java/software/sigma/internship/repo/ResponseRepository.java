f5d68a3f35d429fd93fa5f918eb9d46e45941a91
Natan <59052957+Natanius18@users.noreply.github.com>
2022-07-27 13:43:57 +0300

    EDC-74 Fix wrong number of try (#26)

--- src/main/java/software/sigma/internship/repo/ResponseRepository.java
+++ src/main/java/software/sigma/internship/repo/ResponseRepository.java
@@ -3,10 +3,11 @@ package software.sigma.internship.repo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import software.sigma.internship.entity.Response;
+import software.sigma.internship.entity.Test;
 
 import java.util.Optional;
 
 @Repository
 public interface ResponseRepository extends JpaRepository<Response, Long> {
-    Optional<Response> getFirstByStudentIdOrderByNumberOfTryDesc(Long id);
+    Optional<Response> getFirstByStudentIdAndTestOrderByNumberOfTryDesc(Long studentId, Test test);
 }
