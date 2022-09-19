7361d6c23b47f8f2f8d3e606d4eafc5dc778d133
natan.chachko <natan.chachko@sigma.software>
2022-09-19 13:22:49 +0300

    EDC-89 Refactor entities and enums

--- src/main/java/software/sigma/internship/entity/Teacher.java
+++ src/main/java/software/sigma/internship/entity/Teacher.java
@@ -1,21 +1,21 @@
 package software.sigma.internship.entity;
 
 import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.Hibernate;
 
 import javax.persistence.CascadeType;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 import java.util.List;
+import java.util.Objects;
 
 @Entity
 @Getter
 @Setter
-@EqualsAndHashCode(callSuper = true)
 @NoArgsConstructor
 @AllArgsConstructor
 @DiscriminatorValue("Teacher")
@@ -29,5 +29,18 @@ public class Teacher extends Person {
         HEAD_TEACHER,
         DOCENT,
         PROFESSOR
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Teacher teacher = (Teacher) o;
+        return getId() != null && Objects.equals(getId(), teacher.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
     }
 }
--- src/main/java/software/sigma/internship/enums/Permission.java
+++ src/main/java/software/sigma/internship/enums/Permission.java
@@ -1,20 +1,18 @@
 package software.sigma.internship.enums;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 @AllArgsConstructor
-@Getter
 public enum Permission {
     READ("students:read"),
     READ_FULL("teachers:read"),
     WRITE("writeTest"),
     APPROVE_PROFILE("admin:approveProfile");
 
-    private final String permission;
+    private final String authorityName;
 
     public SimpleGrantedAuthority getAuthority() {
-        return new SimpleGrantedAuthority(permission);
+        return new SimpleGrantedAuthority(authorityName);
     }
 }
