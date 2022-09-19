87cdd9a7f4c8207cd6c220918e338ffc971c32ab
natan.chachko <natan.chachko@sigma.software>
2022-09-19 18:18:59 +0300

    Delete useless equals & hashcode methods

--- src/main/java/software/sigma/internship/entity/Teacher.java
+++ src/main/java/software/sigma/internship/entity/Teacher.java
@@ -4,14 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.Hibernate;
 
 import javax.persistence.CascadeType;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 import java.util.List;
-import java.util.Objects;
 
 @Entity
 @Getter
@@ -29,18 +27,5 @@ public class Teacher extends Person {
         HEAD_TEACHER,
         DOCENT,
         PROFESSOR
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
-        Teacher teacher = (Teacher) o;
-        return getId() != null && Objects.equals(getId(), teacher.getId());
-    }
-
-    @Override
-    public int hashCode() {
-        return getClass().hashCode();
     }
 }
