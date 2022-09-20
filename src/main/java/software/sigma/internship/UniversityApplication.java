de09408a73ec790c932125c26be1ea76963733f2
natan.chachko <natan.chachko@sigma.software>
2022-09-20 10:41:15 +0300

    EDC-85 Add caching

--- src/main/java/software/sigma/internship/UniversityApplication.java
+++ src/main/java/software/sigma/internship/UniversityApplication.java
@@ -2,10 +2,12 @@ package software.sigma.internship;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableCaching
 @EnableSwagger2
 @EnableScheduling
 @SpringBootApplication
